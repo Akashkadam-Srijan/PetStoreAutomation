d8495674d9d89d2b17b78b779833c3fb6116de1a
Akash <akash.kadam@materialplus.io>
2024-10-29 12:44:07 +0530

    New report

--- src/test/java/api/endpoints/PetEndPoints.java
+++ src/test/java/api/endpoints/PetEndPoints.java
@@ -3,7 +3,6 @@ package api.endpoints;
 import static io.restassured.RestAssured.given;
 
 import api.payload.Pet;
-import api.payload.User;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 
